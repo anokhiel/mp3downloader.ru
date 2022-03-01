91f11f03d67fdd2b7ec487636f5808c856f10d9c
Alexander Popov <alexandpopov@yandex.ru>
2022-03-01 12:51:09 +0300

    Readme changed.

--- src/main/java/ru/mp3downloader/model/LinkOrder.java
+++ src/main/java/ru/mp3downloader/model/LinkOrder.java
@@ -18,7 +18,7 @@ import java.time.LocalDateTime;
 @Builder
 public class LinkOrder {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Long id;
 
 
